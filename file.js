f43255d57cdb42c68edcc3718260da0f449d5f21
Mahesh kumar <vickymandal542@gmail.com>
2025-11-01 13:59:33 +0530

    read

---
+++ file.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// READing the file
+const readed_data = fs.readFileSync("./statement.csv","utf-8");
+
+const lines = readed_data.trim().split("\n");
+// console.log(lines);
+
+const heading = lines[0].trim().split(",");
+
+const object = lines.slice(1).map((line)=>{
+        const values = line.trim().split(",");
+        let arr=[];
+        for (let i = 0; i < heading.length; i++) {
+            const key = heading[i].trim();
+            const value = values[i].trim();
+
+            arr[key]=value;
+        }
+        return arr;
+});

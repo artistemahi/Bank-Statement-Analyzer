4929593feb451d606b8d48b4153430b6e9ac6909
Mahesh kumar <vickymandal542@gmail.com>
2025-11-01 15:37:17 +0530

    summary.csv

--- file.js
+++ file.js
@@ -10,7 +10,7 @@ const heading = lines[0].trim().split(",");
 
 const object = lines.slice(1).map((line)=>{
         const values = line.trim().split(",");
-        let arr=[];
+        let arr={};
         for (let i = 0; i < heading.length; i++) {
             const key = heading[i].trim();
             const value = values[i].trim();
@@ -21,17 +21,17 @@ const object = lines.slice(1).map((line)=>{
 });
 
 //SORTing the transaction by date
-    const sorted_data= data.sort((a,b)=>{new Date(a.date)-new Date(b.date)});
+    const sorted_data= object.sort((a,b)=>new Date(a.date)-new Date(b.date));
 
 // SUMMary 
 let summ_arr={};
 sorted_data.forEach(element => {
         const name = element.AccountHolder;
-        const amount = element.Number(element.Amount)
+        const amount = Number(element.Amount)
         const type = element.Type.toLowerCase();
         const remark = element.Remarks.toLowerCase();
-    if (!summ_arr) {
-       summ_arr={
+    if (!summ_arr[name]) {
+       summ_arr[name]={
         AccountHolder:name,
         TotalCredit: 0,
         TotalDebit: 0,
@@ -39,14 +39,27 @@ sorted_data.forEach(element => {
         SalaryTransactions: []
        };
     }
-      if(type=="credit") summ_arr[name].TotalCredit +=amount ;
-      else if(type=="debit") summ_arr[name].TotalDebit+=amount ;
+      if(type==="credit") summ_arr[name].TotalCredit +=amount ;
+      else if(type==="debit") summ_arr[name].TotalDebit+=amount ;
 
-      if(amount >LargestTransaction){
+      if(amount >summ_arr[name].LargestTransaction){
         summ_arr[name].LargestTransaction=amount;
       }
-      if (remark.include("salary")) {
+      if (remark.includes("salary")) {
             summ_arr[name].SalaryTransactions.push(element.TransactionID);        
       }
 
 });
+
+const summary_array=Object.values(summ_arr);
+
+
+const header = "AccountHolder,TotalCredit,TotalDebit,LargestTransaction,SalaryTransactions";
+let csv = header;
+for(let i =0; i < summary_array.length; i++){
+    const user = summary_array[i];
+    csv += `${user.AccountHolder},${user.TotalCredit},${user.TotalDebit},${user.LargestTransaction},${user.SalaryTransactions}` 
+}
+
+
+fs.writeFileSync("./summary.csv",csv);
